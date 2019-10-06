29fcc7d8f4f06c87603112b0c70789ded0f9301a
Галиуллин Ильдар <ldrgllln@gmail.com>
2019-10-07 00:20:02 +0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import org.w3c.dom.*;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            String fileName = "files/rustih(1).xml";
+            Integer index = 9;
+            File fileXml = new File(fileName);
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.parse(fileXml);
+            doc.getDocumentElement().normalize();
+            NodeList lst = doc.getChildNodes();
+            String data;
+            Element child = (Element) lst.item(0).getChildNodes().item(index);
+            System.out.println(child.getFirstChild().getNodeValue());
+        } catch (Exception e){
+            System.out.println(e);
+        }
+    }
+}
